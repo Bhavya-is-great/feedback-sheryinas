0b992f977aa45421f58354457e72d537daaea9a1
Bhavya-is-great <bdisop1234@gmail.com>
2026-03-16 22:17:22 +0530

    analysis

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import ToastProvider from "@/components/ui/ToastProvider";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,6 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Analytics />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ToastProvider>{children}</ToastProvider>
       </body>
